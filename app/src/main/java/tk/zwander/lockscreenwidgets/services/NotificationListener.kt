150b7460af38d1c2281ae31117da52f16998f451
zacharee <zachary.wander@gmail.com>
2020-05-17 20:52:55 -0400

    Check notification importance instead of priority on 8.0+

--- app/src/main/java/tk/zwander/lockscreenwidgets/services/NotificationListener.kt
+++ app/src/main/java/tk/zwander/lockscreenwidgets/services/NotificationListener.kt
@@ -1,12 +1,16 @@
 package tk.zwander.lockscreenwidgets.services
 
 import android.app.Notification
+import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.os.Build
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
+import android.util.Log
+import androidx.core.app.NotificationCompat
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 
 class NotificationListener : NotificationListenerService() {
@@ -33,6 +37,8 @@ class NotificationListener : NotificationListenerService() {
         abstract fun onUpdate(count: Int)
     }
 
+    private val nm by lazy { getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager }
+
     override fun onCreate() {
         sendUpdate()
     }
@@ -47,7 +53,14 @@ class NotificationListener : NotificationListenerService() {
 
     private fun sendUpdate() {
         val intent = Intent(ACTION_NEW_NOTIFICATION_COUNT)
-        intent.putExtra(EXTRA_NOTIFICATION_COUNT, activeNotifications.filter { it.notification.priority > Notification.PRIORITY_MIN }.size)
+        intent.putExtra(EXTRA_NOTIFICATION_COUNT, activeNotifications.filter {
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.N_MR1) {
+                val ranking = Ranking().apply { currentRanking.getRanking(it.key, this) }
+                ranking.importance > NotificationManager.IMPORTANCE_MIN
+            } else {
+                it.notification.priority > Notification.PRIORITY_MIN
+            }
+        }.size)
 
         LocalBroadcastManager.getInstance(this).sendBroadcast(intent)
     }
