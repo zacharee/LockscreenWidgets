7b28354ac2b1ecc1c3bc9a89eb72bfe4f8f15777
zacharee <zachary.wander@gmail.com>
2020-05-30 22:48:47 -0400

    Set 1-dp padding on widget host Views

--- app/src/main/java/tk/zwander/lockscreenwidgets/views/ZeroPaddingAppWidgetHostView.kt
+++ app/src/main/java/tk/zwander/lockscreenwidgets/views/ZeroPaddingAppWidgetHostView.kt
@@ -3,11 +3,13 @@ package tk.zwander.lockscreenwidgets.views
 import android.appwidget.AppWidgetHostView
 import android.appwidget.AppWidgetProviderInfo
 import android.content.Context
+import tk.zwander.lockscreenwidgets.util.dpAsPx
 
 class ZeroPaddingAppWidgetHostView(context: Context) : AppWidgetHostView(context) {
     override fun setAppWidget(appWidgetId: Int, info: AppWidgetProviderInfo?) {
         super.setAppWidget(appWidgetId, info)
 
-        setPadding(0, 0, 0, 0)
+        val oneDp = context.dpAsPx(1)
+        setPadding(oneDp, oneDp, oneDp, oneDp)
     }
 }
