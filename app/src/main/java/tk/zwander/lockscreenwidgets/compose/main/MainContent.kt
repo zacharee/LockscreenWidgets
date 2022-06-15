039de95a4e59abfcdd11b0d369bce800295e3075
Zachary Wander <zachary.wander@gmail.com>
2022-06-14 22:33:20 -0400

    Code cleanup

--- app/src/main/java/tk/zwander/lockscreenwidgets/compose/main/MainContent.kt
+++ app/src/main/java/tk/zwander/lockscreenwidgets/compose/main/MainContent.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.google.android.material.composethemeadapter.MdcTheme
