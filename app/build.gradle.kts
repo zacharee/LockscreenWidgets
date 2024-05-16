2340bf0fbd0feaa3ecca61691ee044543fb319d0
Zachary Wander <zachary.wander@gmail.com>
2024-05-16 18:12:28 -0400

    Version 2.14.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "tk.zwander.lockscreenwidgets"
         minSdk = 22
         targetSdk = 34
-        versionCode = 108
-        versionName = "2.13.5"
+        versionCode = 109
+        versionName = "2.14.0"
 
         extensions.getByType(BasePluginExtension::class.java).archivesName.set("LockscreenWidgets_${versionName}")
     }
