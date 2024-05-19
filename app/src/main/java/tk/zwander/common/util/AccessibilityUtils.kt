d82fc6d8c53ad7fd55a5a4c695f78b1b6507055d
Zachary Wander <zachary.wander@gmail.com>
2024-05-19 19:09:26 -0400

    Cleanup

--- app/src/main/java/tk/zwander/common/util/AccessibilityUtils.kt
+++ app/src/main/java/tk/zwander/common/util/AccessibilityUtils.kt
@@ -397,7 +397,6 @@ object AccessibilityUtils {
             )
 
             newState = frameDelegate.state.copy()
-            newFrameCommonState = frameDelegate.commonState.copy()
 
             //The below block can (very rarely) take over half a second to execute, so only run it
             //if we actually need to (i.e. on the lock screen and screen is on).
@@ -471,7 +470,6 @@ object AccessibilityUtils {
                         wm = wm,
                         updateAccessibility = true,
                         transform = { newState },
-                        commonTransform = { newFrameCommonState },
                     )
                 }
 
