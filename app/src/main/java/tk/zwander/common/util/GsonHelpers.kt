3a2440075c2fc1b20f26054ee1280be3c840a51e
Zachary Wander <zachary.wander@gmail.com>
2024-12-25 15:55:29 -0500

    Better null handling for custom de/serializers

--- app/src/main/java/tk/zwander/common/util/GsonHelpers.kt
+++ app/src/main/java/tk/zwander/common/util/GsonHelpers.kt
@@ -31,33 +31,38 @@ class CrashFixExclusionStrategy : ExclusionStrategy {
     }
 }
 
-class GsonUriHandler : JsonDeserializer<Uri>, JsonSerializer<Uri> {
+class GsonUriHandler : JsonDeserializer<Uri?>, JsonSerializer<Uri?> {
     override fun deserialize(
-        src: JsonElement, srcType: Type,
-        context: JsonDeserializationContext
+        src: JsonElement?,
+        srcType: Type,
+        context: JsonDeserializationContext?
     ): Uri? {
-        return try {
-            Uri.parse(src.asString)
-        } catch (e: Exception) {
-            null
+        return src?.let {
+            try {
+                Uri.parse(src.asString)
+            } catch (e: Exception) {
+                null
+            }
         }
     }
 
-    override fun serialize(src: Uri, typeOfSrc: Type, context: JsonSerializationContext): JsonElement {
-        return JsonPrimitive(src.toString())
+    override fun serialize(src: Uri?, typeOfSrc: Type, context: JsonSerializationContext): JsonElement {
+        return JsonPrimitive(src?.toString())
     }
 }
 
-class GsonIntentHandler : JsonSerializer<Intent>, JsonDeserializer<Intent> {
-    override fun serialize(src: Intent, typeOfSrc: Type?, context: JsonSerializationContext?): JsonElement {
-        return JsonPrimitive(src.toUri(0))
+class GsonIntentHandler : JsonSerializer<Intent?>, JsonDeserializer<Intent?> {
+    override fun serialize(src: Intent?, typeOfSrc: Type?, context: JsonSerializationContext?): JsonElement {
+        return JsonPrimitive(src?.toUri(0))
     }
 
-    override fun deserialize(json: JsonElement, typeOfT: Type?, context: JsonDeserializationContext?): Intent? {
-        return try {
-            Intent.parseUri(json.asString, 0)
-        } catch (e: Exception) {
-            null
+    override fun deserialize(json: JsonElement?, typeOfT: Type?, context: JsonDeserializationContext?): Intent? {
+        return json?.let {
+            try {
+                Intent.parseUri(json.asString, 0)
+            } catch (e: Exception) {
+                null
+            }
         }
     }
 }
