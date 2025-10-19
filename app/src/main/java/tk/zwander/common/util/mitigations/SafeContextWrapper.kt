d801d8901d6382e032b25b613e21acb24863a468
Zachary Wander <zachary.wander@gmail.com>
2025-10-19 13:03:46 -0400

    Catch errors when registering receivers

--- app/src/main/java/tk/zwander/common/util/mitigations/SafeContextWrapper.kt
+++ app/src/main/java/tk/zwander/common/util/mitigations/SafeContextWrapper.kt
@@ -1,8 +1,14 @@
 package tk.zwander.common.util.mitigations
 
+import android.Manifest
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.ContextWrapper
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Handler
+import android.os.UserHandle
+import androidx.annotation.RequiresPermission
 import tk.zwander.common.util.logUtils
 
 open class SafeContextWrapper(context: Context) : ContextWrapper(context) {
@@ -11,6 +17,52 @@ open class SafeContextWrapper(context: Context) : ContextWrapper(context) {
             super.unregisterReceiver(receiver)
         } catch (e: Exception) {
             logUtils.debugLog("Unable to unregister receiver.", e)
+        }
+    }
+
+    @RequiresPermission(Manifest.permission.INTERACT_ACROSS_USERS_FULL)
+    override fun registerReceiverAsUser(
+        receiver: BroadcastReceiver?,
+        user: UserHandle?,
+        filter: IntentFilter?,
+        broadcastPermission: String?,
+        scheduler: Handler?,
+    ): Intent? {
+        return try {
+            super.registerReceiverAsUser(
+                receiver,
+                user,
+                filter,
+                broadcastPermission,
+                scheduler,
+            )
+        } catch (e: Throwable) {
+            logUtils.normalLog("Unable to register receiver.", e)
+            null
+        }
+    }
+
+    @RequiresPermission(Manifest.permission.INTERACT_ACROSS_USERS_FULL)
+    override fun registerReceiverAsUser(
+        receiver: BroadcastReceiver?,
+        user: UserHandle?,
+        filter: IntentFilter?,
+        broadcastPermission: String?,
+        scheduler: Handler?,
+        flags: Int,
+    ): Intent? {
+        return try {
+            super.registerReceiverAsUser(
+                receiver,
+                user,
+                filter,
+                broadcastPermission,
+                scheduler,
+                flags,
+            )
+        } catch (e: Throwable) {
+            logUtils.normalLog("Unable to register receiver.", e)
+            null
         }
     }
 }
