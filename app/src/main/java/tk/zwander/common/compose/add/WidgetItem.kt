f449b8bcbf68923cc13d71bd5aada5a41c3fe3e2
Zachary Wander <zachary.wander@gmail.com>
2025-11-15 10:52:15 -0500

    Put click layout on top of widget previews in widget scroller to prevent accidental clicks on widget previews

--- app/src/main/java/tk/zwander/common/compose/add/WidgetItem.kt
+++ app/src/main/java/tk/zwander/common/compose/add/WidgetItem.kt
@@ -3,6 +3,7 @@ package tk.zwander.common.compose.add
 import android.graphics.drawable.Drawable
 import android.view.View
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -29,52 +30,62 @@ fun WidgetItem(
     onClick: () -> Unit,
 ) {
     OutlinedCard(
-        onClick = onClick,
-        border = CardDefaults.outlinedCardBorder(true)
+        border = CardDefaults.outlinedCardBorder(true),
     ) {
-        Column(
-            modifier = Modifier
-                .size(200.dp)
-                .padding(8.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(
-                text = label ?: "",
-                textAlign = TextAlign.Center
-            )
-            Text(
-                text = subLabel ?: "",
-                fontSize = 12.sp,
-                textAlign = TextAlign.Center
-            )
+        Box {
+            Column(
+                modifier = Modifier
+                    .size(200.dp)
+                    .padding(8.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text(
+                    text = label ?: "",
+                    textAlign = TextAlign.Center,
+                )
 
-            Spacer(Modifier.size(8.dp))
+                Text(
+                    text = subLabel ?: "",
+                    fontSize = 12.sp,
+                    textAlign = TextAlign.Center,
+                )
 
-            when {
-                previewLayout != null -> {
-                    Box(
-                        modifier = Modifier.weight(1f),
-                        contentAlignment = Alignment.Center,
-                    ) {
-                        AndroidView(
-                            factory = { previewLayout },
-                            update = {},
+                Spacer(Modifier.size(8.dp))
+
+                when {
+                    previewLayout != null -> {
+                        Box(
+                            modifier = Modifier.weight(1f),
+                            contentAlignment = Alignment.Center,
+                        ) {
+                            AndroidView(
+                                factory = { previewLayout },
+                                update = {},
+                            )
+                        }
+                    }
+
+                    image != null -> {
+                        Image(
+                            painter = rememberDrawablePainter(image),
+                            contentDescription = label,
+                            modifier = Modifier.weight(1f),
                         )
                     }
-                }
 
-                image != null -> {
-                    Image(
-                        painter = rememberDrawablePainter(image),
-                        contentDescription = label,
-                        modifier = Modifier.weight(1f)
-                    )
-                }
-
-                else -> {
-                    Spacer(Modifier.weight(1f))
+                    else -> {
+                        Spacer(Modifier.weight(1f))
+                    }
                 }
             }
+
+            Box(
+                modifier = Modifier
+                    .matchParentSize()
+                    .clickable(
+                        onClick = onClick,
+                    ),
+            )
         }
     }
 }
