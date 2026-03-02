9cbe99f8ddacb3f33f63118cb763d22d12126ee7
Zachary Wander <zachary.wander@gmail.com>
2026-03-02 00:12:15 -0500

    Compare key set size when matching Bundles

--- app/src/main/java/tk/zwander/common/util/BundleUtils.kt
+++ app/src/main/java/tk/zwander/common/util/BundleUtils.kt
@@ -3,7 +3,12 @@ package tk.zwander.common.util
 import android.os.Bundle
 
 fun Bundle.matches(other: Bundle): Boolean {
+    if (this.keySet().size != other.keySet().size) {
+        return false
+    }
+
     this.keySet().forEach { key ->
+        @Suppress("DEPRECATION")
         if (this[key] != other[key]) {
             return false
         }
