a7843e20609a9a0ec2826b8dbaf8cd3dd96a7c42
Zachary Wander <zachary.wander@gmail.com>
2026-03-02 11:07:50 -0500

    Version 4.0.0-alpha10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "tk.zwander.lockscreenwidgets"
         minSdk = 23
         targetSdk = 36
-        versionCode = 182
-        versionName = "4.0.0-alpha09"
+        versionCode = 183
+        versionName = "4.0.0-alpha10"
 
         manifestPlaceholders["build_uuid"] = UUID.nameUUIDFromBytes("InstallWithOptions_${versionCode}".toByteArray()).toString()
 
