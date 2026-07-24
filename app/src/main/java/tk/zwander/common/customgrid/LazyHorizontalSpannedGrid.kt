75eba558f95dbb8db1c9401d1bd9e37a5425df5b
Zachary Wander <zachary.wander@gmail.com>
2026-07-24 14:38:50 -0400

    Combine spanned grid code

--- app/src/main/java/tk/zwander/common/customgrid/LazyHorizontalSpannedGrid.kt
+++ app/src/main/java/tk/zwander/common/customgrid/LazyHorizontalSpannedGrid.kt
@@ -1,18 +1,10 @@
 package tk.zwander.common.customgrid
 
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.gestures.FlingBehavior
 import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.lazy.layout.LazyLayout
-import androidx.compose.foundation.lazy.layout.LazyLayoutMeasurePolicy
-import androidx.compose.foundation.lazy.layout.LazyLayoutPrefetchState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalLayoutDirection
-import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 
 /**
@@ -39,47 +31,15 @@ fun LazyHorizontalSpannedGrid(
     require(rowCount > 0) { "rowCount must be positive, was $rowCount" }
     require(columnCount > 0) { "columnCount must be positive, was $columnCount" }
 
-    val itemProviderLambda = rememberLazySpannedGridItemProviderLambda(state, content)
-    val layoutDirection = LocalLayoutDirection.current
-    val placementCache = remember { SpannedGridPlacementCache() }
-    @OptIn(ExperimentalFoundationApi::class)
-    val prefetchState = remember { LazyLayoutPrefetchState() }
-    @OptIn(ExperimentalFoundationApi::class)
-    state.prefetchState = prefetchState
-
-    val measurePolicy =
-        remember(rowCount, columnCount, contentPadding, layoutDirection, state) {
-            LazyLayoutMeasurePolicy { constraints ->
-                measureSpannedGrid(
-                    measureScope = this,
-                    itemProvider = itemProviderLambda(),
-                    state = state,
-                    orientation = Orientation.Horizontal,
-                    crossAxisCount = rowCount,
-                    mainAxisLineCount = columnCount,
-                    contentPadding = contentPadding,
-                    layoutDirection = layoutDirection,
-                    constraints = constraints,
-                    placementCache = placementCache,
-                )
-            }
-        }
-
-    LazyLayout(
-        itemProvider = itemProviderLambda,
-        modifier =
-            modifier.scrollable(
-                state = state,
-                orientation = Orientation.Horizontal,
-                enabled = userScrollEnabled,
-                // Items are placed with placeRelative, which already mirrors x for RTL. A
-                // physical drag in a given screen direction therefore corresponds to the
-                // opposite logical (index) direction in RTL vs. LTR, so the delta needs an
-                // extra flip here to keep "content follows finger" scrolling natural in RTL.
-                reverseDirection = layoutDirection == LayoutDirection.Rtl,
-                flingBehavior = flingBehavior,
-            ),
-        prefetchState = prefetchState,
-        measurePolicy = measurePolicy,
+    LazySpannedGrid(
+        mainAxisCount = columnCount,
+        crossAxisCount = rowCount,
+        orientation = Orientation.Horizontal,
+        modifier = modifier,
+        state = state,
+        contentPadding = contentPadding,
+        userScrollEnabled = userScrollEnabled,
+        flingBehavior = flingBehavior,
+        content = content,
     )
 }
--- app/src/main/java/tk/zwander/common/customgrid/LazyVerticalSpannedGrid.kt
+++ app/src/main/java/tk/zwander/common/customgrid/LazyVerticalSpannedGrid.kt
@@ -1,17 +1,10 @@
 package tk.zwander.common.customgrid
 
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.gestures.FlingBehavior
 import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.lazy.layout.LazyLayout
-import androidx.compose.foundation.lazy.layout.LazyLayoutMeasurePolicy
-import androidx.compose.foundation.lazy.layout.LazyLayoutPrefetchState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.unit.dp
 
 /**
@@ -42,43 +35,15 @@ fun LazyVerticalSpannedGrid(
     require(columnCount > 0) { "columnCount must be positive, was $columnCount" }
     require(rowCount > 0) { "rowCount must be positive, was $rowCount" }
 
-    val itemProviderLambda = rememberLazySpannedGridItemProviderLambda(state, content)
-    val layoutDirection = LocalLayoutDirection.current
-    val placementCache = remember { SpannedGridPlacementCache() }
-    @OptIn(ExperimentalFoundationApi::class)
-    val prefetchState = remember { LazyLayoutPrefetchState() }
-    @OptIn(ExperimentalFoundationApi::class)
-    state.prefetchState = prefetchState
-
-    val measurePolicy =
-        remember(columnCount, rowCount, contentPadding, layoutDirection, state) {
-            LazyLayoutMeasurePolicy { constraints ->
-                measureSpannedGrid(
-                    measureScope = this,
-                    itemProvider = itemProviderLambda(),
-                    state = state,
-                    orientation = Orientation.Vertical,
-                    crossAxisCount = columnCount,
-                    mainAxisLineCount = rowCount,
-                    contentPadding = contentPadding,
-                    layoutDirection = layoutDirection,
-                    constraints = constraints,
-                    placementCache = placementCache,
-                )
-            }
-        }
-
-    LazyLayout(
-        itemProvider = itemProviderLambda,
-        modifier =
-            modifier.scrollable(
-                state = state,
-                orientation = Orientation.Vertical,
-                enabled = userScrollEnabled,
-                reverseDirection = false,
-                flingBehavior = flingBehavior,
-            ),
-        prefetchState = prefetchState,
-        measurePolicy = measurePolicy,
+    LazySpannedGrid(
+        mainAxisCount = rowCount,
+        crossAxisCount = columnCount,
+        orientation = Orientation.Vertical,
+        modifier = modifier,
+        state = state,
+        contentPadding = contentPadding,
+        userScrollEnabled = userScrollEnabled,
+        flingBehavior = flingBehavior,
+        content = content,
     )
 }
